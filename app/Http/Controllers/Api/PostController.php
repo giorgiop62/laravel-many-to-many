671e153478eab58268177057bebb69cc90c65b29
Giorgio Perez <giorgiop62@gmail.com>
2023-01-29 22:31:36 +0100

    primo commit

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+use App\Models\Post;
+
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(){
+
+        $posts = Post::all();
+       return response()->json(compact('posts'));
+    }
+}
